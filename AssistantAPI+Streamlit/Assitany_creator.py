0c1d2570613636e88618970eb7f797f456041e5a
jiapn123 <155503641+jiapn123@users.noreply.github.com>
2024-04-17 12:22:23 +0900

    Add files via upload

---
+++ AssistantAPI+Streamlit/Assitany_creator.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+client = OpenAI(api_key="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")
+
+file="/Users/jiapannan/PycharmProjects/pythonProject/pythonProject/1000 Assistant API/assistant_api/SeoulTourismPlaces.html"
+
+def file_upload(file):
+    file = client.files.create(
+        file=open(file, "rb"),
+        purpose="assistants")
+    
+    # print(file)  # file-6Dd5teLybO6sqa9yJa53dZeq
+    return file.id
+
+# file_id = file_upload(file)        
+    
+def assistant_creator():
+    my_assistant = client.beta.assistants.create(
+        instructions="""
+        TO-DO: 
+        - You are an expert in making travel plans for Seoul based on your LLM model and the given Retrieval file SeoulTourismPlaces.html. 
+        - For every suggested place that you've mentioned in the reply message, you will add a clickable link to the suggested places by using the links in the SeoulTourismPlaces.html.
+
+        Expected outcome:
+        - A day-to-day travel internity for planning request
+        -further explore links provided for suggested places by using the given file SeoulTourismPlaces.html  
+        - When the user asks for additional information about a place, provide the full url address from the SeoulTourismPlaces.html and give a short summary
+        - Output example: 
+        "1일차:첫째 날은 서울의 대표적인 자연 경관 중 하나인 남산을 방문하여 서울의 전경을 조망해 보세요. [남산 서울타워 정보 보러가기](explorer_URL for 남산 서울타워)
+
+        About SeoulTourismPlaces.html: 
+        - For the provided Retrieval file SeoulTourismPlaces.html, it contains 3 types of information: place_name,  explorer_URL, and service_type.
+        - The service_name is the hotels, attractions, and restaurant names in Seoul
+        - The explorer_URL is a webpage URL to help users explore additional information. When the user accesses the website can get up-to-date information and increase the visibility of a place. 
+        - The service_type identifies whether the place is an attraction, restaurant, or hotel. 
+        - the restaurant_menu is special for restaurants 
+        - the tag_of_attraction is special for attraction and has identified the attraction's features     
+        """,
+        name="Travel Panda Seoul Expert",
+        tools=[{"type": "retrieval"}],
+        model="gpt-4-1106-preview",
+        file_ids=["file-6Dd5teLybO6sqa9yJa53dZeq"],
+    )
+    print(my_assistant) # asst_FPlHYRozJhMRSNBZoC8qgusB
+    
+    return my_assistant.id
+
+# my_assistant_id = assistant_creator()
+
+empty_thread = client.beta.threads.create()
+print(empty_thread) # thread_NhwJvYxk1zDrs5CzyyLO8q4s
+
---
+++ AssistantAPI+Streamlit/streamlit_openai.py
@@ -0,0 +1,81 @@
+from openai import OpenAI
+import streamlit as st
+import time
+
+assistant_id = "asst_hgAdGncKbpUrxLCnSjlv6Mef"
+# thread_id = "thread_g4iieAcxug12JyiFFdpM5rDY"
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password",value="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")
+    
+    client = OpenAI(api_key=openai_api_key)
+    
+    thread_id = st.text_input("Thread ID")
+    thread_btn = st.button("Create a new thread")
+    
+    if thread_btn: 
+        thread = client.beta.threads.create()
+        thread_id = thread.id # thread_NhwJvYxk1zDrs5CzyyLO8q4s
+        
+        st.subheader(f"{thread_id}",divider="rainbow")
+        st.info("스레드가 생성되었습니다.")
+        
+        
+st.title("💬 Travel Panda Seoul Expert")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "서울 여행 같이 가볼까요?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+        
+    if not thread_id:
+        st.info("Please add your thread ID to continue.")
+        st.stop()
+
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    
+    response = client.beta.threads.messages.create(
+        thread_id,
+        role="user",
+        content=prompt,
+    )
+    # print(response)
+    
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id
+    )
+    print(run)
+    run_id = run.id
+    
+    while True:
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run_id
+        )
+        if run.status == "completed":
+            break
+        else:
+            time.sleep(2)
+        print(run)
+        
+    thread_messages = client.beta.threads.messages.list(thread_id)
+    #print(thread_messages.data)
+    
+    msg = thread_messages.data[0].content[0].text.value
+    # print(msg)
+    
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
+    
+
+
+
